98c733796702f5d26f6777e2f08039a282781710
Joseph Watkins <joseph.o.watkins@gmail.com>
2018-12-22 14:58:18 +0000

    fix adding bars to the bar chart

--- src-cljs/reagent_d3/bar_chart.cljs
+++ src-cljs/reagent_d3/bar_chart.cljs
@@ -2,12 +2,15 @@
   (:require [d3 :as d3]
             [reagent.core :as reagent]))
 
+(def initial-state [7 10 13 19 21 25 22 18 15 13 11 12 15 20 18 17 16 18 23 25])
+
 (defonce chart-state
-  (reagent/atom [2 10 13 19 21 25 22 18 15 13 11 12 15 20 18 17 16 18 23 25]))
+  (reagent/atom initial-state))
 
 (def width 600)
 (def height 250)
 (def bar-padding 1)
+(def duration 750)
 
 (defn draw [state]
   (let [svg (d3/select ".bar-chart__chart")
@@ -18,7 +21,11 @@
                     (.paddingInner 0.05))
         y-scale (-> (d3/scaleLinear)
                     (.domain #js [0 (d3/max data)])
-                    (.range #js [0 height]))]
+                    (.range #js [0 height]))
+        label-y (fn [d] (let [bar-height (y-scale d)]
+                          (if (> bar-height 20)
+                            (-> height (- bar-height) (+ 14))
+                            (-> height (- bar-height) (- 7)))))]
     ;; create bars
     (-> svg
         (.selectAll "rect")
@@ -37,13 +44,9 @@
         (.enter)
         (.append "text")
         (.text (fn [d] d))
-        (.attr "text-anchor" "middle")
         (.attr "x" (fn [d i] (+ (x-scale i) (/ (.bandwidth x-scale) 2))))
-        (.attr "y" (fn [d]
-                     (let [bar-height (y-scale d)]
-                       (if (> bar-height 20)
-                         (-> height (- bar-height) (+ 14))
-                         (-> height (- bar-height) (- 7))))))
+        (.attr "y" label-y )
+        (.attr "text-anchor" "middle")
         (.attr "font-size" "11px")
         (.attr "fill" (fn [d] (if (> (y-scale d) 20) "white" "black"))))))
 
@@ -54,35 +57,73 @@
                     (.domain (d3/range (count data)))
                     (.rangeRound #js [0 width])
                     (.paddingInner 0.05))
+        x-scale-extended (fn [n]
+                           (let [[q r] ((juxt quot rem) n (count data))]
+                             (+ (* width q) (x-scale r))))
         y-scale (-> (d3/scaleLinear)
                     (.domain #js [0 (d3/max data)])
-                    (.range #js [0 height]))]
+                    (.range #js [0 height]))
+        label-y (fn [d]
+                  (let [bar-height (y-scale d)]
+                    (if (> bar-height 20)
+                      (-> height (- bar-height) (+ 14))
+                      (-> height (- bar-height) (- 7)))))
+        bars (-> svg
+                 (.selectAll "rect")
+                 (.data data))
+        labels (-> svg
+                   (.selectAll "text")
+                   (.data data))]
     ;; re-draw bars
-    (-> svg
-        (.selectAll "rect")
-        (.data data)
-        (.transition)
-        (.duration 1000)
+    (-> bars
+        (.enter)
+        (.append "rect")
+        (.attr "x" width)
         (.attr "y" (fn [d] (- height (y-scale d))))
+        (.attr "width" (.bandwidth x-scale))
+        (.attr "height" (fn [d] (y-scale d)))
+        (.attr "fill" (fn [d] (str "rgb(0,0," (* d 10) ")")))
+        (.merge bars)
+        (.transition)
+        (.duration duration)
+        (.attr "x" (fn [d i] (x-scale i)))
+        (.attr "y" (fn [d] (- height (y-scale d))))
+        (.attr "width" (.bandwidth x-scale))
         (.attr "height" (fn [d] (y-scale d)))
         (.attr "fill" (fn [d] (str "rgb(0,0," (* d 10) ")"))))
-    ;; create labels
-    (-> svg
-        (.selectAll "text")
-        (.data data)
+    ;; remove exiting bars
+    (-> bars
+        (.exit)
         (.transition)
-        (.duration 1000)
+        (.duration duration)
+        (.attr "x" (fn [d i]
+                     (x-scale-extended i)))
+        (.attr "width" (.bandwidth x-scale))
+        (.remove))
+    ;; re-draw labels
+    (-> labels
+        (.enter)
+        (.append "text")
+        (.text (fn [d] d))
+        (.attr "x" (+ width (/ (.bandwidth x-scale) 2)))
+        (.attr "y" label-y)
         (.attr "text-anchor" "middle")
+        (.attr "font-size" "11px")
+        (.attr "fill" (fn [d] (if (> (y-scale d) 20) "white" "black")))
+        (.merge labels)
+        (.transition)
+        (.duration duration)
         (.text (fn [d] d))
         (.attr "x" (fn [d i] (+ (x-scale i) (/ (.bandwidth x-scale) 2))))
-        (.attr "y" (fn [d]
-                     (let [bar-height (y-scale d)]
-                       (if (> bar-height 20)
-                         (-> height (- bar-height) (+ 14))
-                         (-> height (- bar-height) (- 7))))))
-        (.attr "fill" (fn [d] (if (> (y-scale d) 20) "white" "black"))))))
-
-
+        (.attr "y" label-y)
+        (.attr "fill" (fn [d] (if (> (y-scale d) 20) "white" "black"))))
+    ;; remove exiting labels
+    (-> labels
+        (.exit)
+        (.transition)
+        (.duration duration)
+        (.attr "x" (fn [d i] (+ (x-scale-extended i) (/ (.bandwidth x-scale) 2))))
+        (.remove))))
 
 (defn bar-chart [state]
   (reagent/create-class {:display-name "bar chart"
@@ -95,19 +136,30 @@
                                            [:svg.bar-chart__chart
                                             {:height height
                                              :width width}])}))
+(defn add-bar [ratom]
+  [:button.bar-chart__add-item
+   {:on-click #(swap! ratom conj (rand-int 25))}
+   "+ bar"])
 
 (defn randomize! [ratom]
-  (let [new-data (take 20 (repeatedly #(rand-int 25)))]
-    (reset! ratom new-data)))
+  (swap! ratom (fn [state]
+                 (vec (take (count state) (repeatedly #(rand-int 25)))))))
 
-(defn button [ratom]
+(defn randomize [ratom]
   [:button.bar-chart__button
    {:on-click #(randomize! ratom)}
-   "Randomize!"])
+   "randomize!"])
+
+(defn reset [ratom]
+  [:button.bar-chart__button
+   {:on-click #(reset! ratom initial-state)}
+   "reset"])
 
 (defn view []
   [:div.bar-chart
    [:div.bar-chart__title
     [:h2 "Bar chart"]]
-   [button chart-state]
+   [reset chart-state]
+   [randomize chart-state]
+   [add-bar chart-state]
    [bar-chart @chart-state]])
